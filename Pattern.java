5f700d386a02f3118a4a1b557c90f49199d14e7e
swati-208 <122670743+swati-208@users.noreply.github.com>
2026-03-14 12:45:46 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -1,13 +1,21 @@
 class Pattern {
-    public static void main(String args[]) {
-        
+    public static void main(String[] args) {
+
         int num = 1;
 
-        for(int i = 1; i <= 5; i++) {  
-            for(int j = 1; j <= i; j++) {  
-                System.out.print(num + " ");
+        for(int i = 1; i <= 5; i++) {
+
+            for(int j = 1; j <= i; j++) {
+
+                System.out.print(num);
+
+                if(j < i) {
+                    System.out.print(" ");
+                }
+
                 num++;
             }
+
             System.out.println();
         }
     }
