3980179a28821ee04a0ad935cfdafa4d73ed4d7d
HP <swatisolankhe@gmail.com>
2026-03-14 11:58:34 +0530

    Commit all

---
+++ Test002Pattern.java
@@ -0,0 +1,29 @@
+class Test002Pattern{
+
+
+
+    public static void main(String[] args) {
+
+        int rows = 5;
+
+        for(int i = 1; i <= rows; i++) {
+
+            // print spaces
+            for(int j = 1; j <= rows - i; j++) {
+                System.out.print("  ");
+            }
+
+            // increasing numbers
+            for(int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+
+            // decreasing numbers
+            for(int j = i - 1; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
